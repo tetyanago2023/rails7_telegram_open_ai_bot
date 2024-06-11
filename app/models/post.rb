cb44299c62c9dfc8300c943a9c618ec585268002
go2020 <tetyanago2018@gmail.com>
2024-06-11 16:14:03 -0700

    telegram stuff was created

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  after_create :tell_telegram_bot
+
+  def tell_telegram_bot
+    bot = Rails.application.config.telegram_bot
+    token = Rails.application.credentials.dig(:telegram_bot)
+    chat_id = '-4256314609'
+
+    url = Rails.application.routes.url_helpers.post_url(self, host: 'https://0.0.0.0:3000')
+
+    kb = [[
+            Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Read Post', url: url),
+          ]]
+
+    markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+
+    bot.api.send_message(chat_id: chat_id, text: "New post created: *#{title}*", reply_markup: markup, parse_mode: 'Markdown')
+  end
 end
---
+++ config/initializers/telegram_bot.rb
@@ -0,0 +1,26 @@
+require 'telegram/bot'
+
+Rails.application.config.after_initialize do
+  token = Rails.application.credentials.dig(:telegram_bot)
+
+  Telegram::Bot::Client.run(token) do |bot|
+    Rails.application.config.telegram_bot = bot
+    bot.api.get_updates(offset: -1)
+  end
+rescue Telegram::Bot::Exceptions::ResponseError => e
+  Rails.logger.error e
+  Rails.application.config.telegram_bot.stop
+  Rails.application.config.telegram_bot.api.delete_webhook
+end
+
+Signal.trap("TERM") do
+  puts "Shutting down bot..."
+  Rails.application.config.telegram_bot.stop
+  exit
+end
+
+Signal.trap("INT") do
+  puts "Shutting down bot..."
+  Rails.application.config.telegram_bot.stop
+  exit
+end
